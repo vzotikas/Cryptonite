86abe5264a882d462404305f1a1df895769fde95
Administrator <vasium@hotmail.com>
2022-08-19 17:18:50 +0200

    Initial Commit

---
+++ Cryptonite/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cryptonite
+//
+//  Created by Administrator on 8/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cryptonite/CryptoniteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoniteApp.swift
+//  Cryptonite
+//
+//  Created by Administrator on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoniteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
